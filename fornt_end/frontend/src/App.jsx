ffecd05285673d0d66695c36dad7720d24616002
Amrutha <amruthaskashyap4@gmail.com>
2026-05-06 16:52:41 +0530

    Add files via upload

---
+++ fornt_end/frontend/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Student Registration</h1>
+
+      <input type="text" placeholder="Enter Name" /><br /><br />
+
+      <input type="email" placeholder="Enter Email" /><br /><br />
+
+      <input type="text" placeholder="Enter Roll Number" /><br /><br />
+
+      <button>Submit</button>
+    </div>
+  );
+}
+
+export default App;
